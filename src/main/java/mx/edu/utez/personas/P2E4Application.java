7c5c30ad42cdd909dc0b547df159561a45c62288
CokerAlcocer <20203tn066@utez.edu.mx>
2023-03-09 15:29:10 -0600

    [FIRST COMMIT] Primer commit del proyecto

---
+++ src/main/java/mx/edu/utez/personas/P2E4Application.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.personas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2E4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P2E4Application.class, args);
+    }
+
+}
